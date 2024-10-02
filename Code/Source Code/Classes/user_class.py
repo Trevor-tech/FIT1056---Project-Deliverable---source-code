135ee02204444edd01d485ab65688f315c35fb50
ywai0005 <ywai0005@student.monash.edu>
2024-10-02 23:40:16 +0800

    Update user_class.py

---
+++ Code/Source Code/Classes/user_class.py
@@ -0,0 +1,12 @@
+#user_class.py
+
+class User:
+  def __init__(self, username, email, password, role):
+     """
+     Initialize a User object.
+     
+     Args:
+       username (str): The username of the user.
+       password (str): The password of the user.
+       role (str): The role of the user (either 'student' or 'staff').
+    """
