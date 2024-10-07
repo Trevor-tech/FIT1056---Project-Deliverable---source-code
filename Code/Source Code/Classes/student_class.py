9b5452eada13f7978a5eac18ef27705e74b4d6e7
ywai0005 <ywai0005@student.monash.edu>
2024-10-08 07:25:37 +0800

    Added authenticate for student

--- Code/Source Code/Classes/student_class.py
+++ Code/Source Code/Classes/student_class.py
@@ -1,9 +1,41 @@
-from Classes.user_class import User
+import os
+from classes.user_class import User
 
 class Student(User):
+    
+    @staticmethod
+    def authenticate(input_username_or_email, input_password):
+        """
+        Method to authenticate a Student user.
+
+        Parameter(s):
+        - input_username_or_email: str
+        - input_password: str
+
+        Returns:
+        - an instance of Student corresponding to the username or email if successful,
+          None otherwise
+        """
+        recept_path = "./Data/students.txt"
+        if os.path.exists(recept_path):
+            with open(recept_path, "r", encoding="utf8") as rf:
+                lines = rf.readlines()
+            for line in lines:
+                # Sequence unpacking: 
+                # https://docs.python.org/3/tutorial/datastructures.html#tuples-and-sequences
+                username, email, password, role, recept_id, salary, staff_info = line.strip("\n").split(",")
+                
+                if input_username_or_email == username or email:
+                    if input_password == password:
+                        return Student(username, email, password, role, recept_id, salary, staff_info)
+                    else:
+                        return None # or return, or break
+        else:
+            print(f"Please check subdirectory and file {recept_path} exists.")
+        pass
     def __init__(self, username, email, password, student_ID):
         super().__init__(username, email, password)
-        self.__student_ID = student_ID
+        self.student_ID = student_ID
 
     def submit_assignment(self):
         pass
