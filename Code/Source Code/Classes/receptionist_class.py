f05860c62a5fd97e1c76b666e0f446b3a7718e7f
ywai0005 <ywai0005@student.monash.edu>
2024-10-07 08:46:30 +0800

    Update receptionist_class.py

--- Code/Source Code/Classes/receptionist_class.py
+++ Code/Source Code/Classes/receptionist_class.py
@@ -1,6 +1,35 @@
 from Classes.staff_class import Staff
 
 class Receptionist(Staff):
+    def authenticate(input_username_or_email, input_password):
+        """
+        Method to authenticate a Receptionist user.
+
+        Parameter(s):
+        - input_username_or_email: str
+        - input_password: str
+
+        Returns:
+        - an instance of Receptionist corresponding to the username or email if successful,
+          None otherwise
+        """
+        recept_path = "./Data/receptionist.txt"
+        if os.path.exists(recept_path):
+            with open(recept_path, "r", encoding="utf8") as rf:
+                lines = rf.readlines()
+            for line in lines:
+                # Sequence unpacking: 
+                # https://docs.python.org/3/tutorial/datastructures.html#tuples-and-sequences
+                recept_id, first_name, last_name, contact_num, username, password = line.strip("\n").split(",")
+                
+                if input_username == username:
+                    if input_password == password:
+                        return ReceptionistUser(recept_id, first_name, last_name, contact_num, input_username, input_password)
+                    else:
+                        return None # or return, or break
+        else:
+            print(f"Please check subdirectory and file {recept_path} exists.")
+            
     def __init__(self, username, email, password, role, staff_type, staff_ID, salary, staff_info):
         super().__init__(username, email, password, role, staff_type, staff_ID, salary, staff_info)
 
@@ -13,3 +42,4 @@ if __name__ == "__main__":
     print(receptionist.staff_type)
     print(receptionist.staff_ID)
     print(receptionist.salary)
+    pass
