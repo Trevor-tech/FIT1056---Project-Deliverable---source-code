6bff7af9bf8024cfce2350cb4b46dbaba147f4ab
ywai0005 <ywai0005@student.monash.edu>
2024-10-18 11:35:38 +0800

    Added TeacherSalary & TestCases
    payment_class.py;
      Added class TeacherSalary:
        - __init__
        - import_teachers_data
        - teacher_salary

--- Code/Source Code/Classes/payment_class.py
+++ Code/Source Code/Classes/payment_class.py
@@ -1,16 +1,42 @@
+import os
+import sys
+
 from classes.receptionist_class import Receptionist
 
-class Payment(Receptionist):
-    def __init__(self, username, email, password, role, staff_type, staff_ID, salary, staff_info):
-        super().__init__(username, email, password, role, staff_type, staff_ID, salary, staff_info)
+class TeacherSalary:
+    teachers_path = "./Data/teachers.txt"
 
-if __name__ == "__main__":  
-    payment = Payment("John Doe", "john.doe@example.com", "password123", "payment", "Payment", "P001", 50000, "Payment of Mathematics")
-    print(payment.username)
-    print(payment.email)
-    print(payment.password)
-    print(payment.role)
-    print(payment.staff_type)
-    print(payment.staff_ID)
-    print(payment.salary)
-    print(payment.staff_info)
+    def __init__(self, username, email, recept_id, salary, staff_info):
+        self.username = username
+        self.email = email
+        self.recept_id = recept_id
+        self.salary = salary
+        self.staff_info = staff_info
+    
+    def import_teachers_data(self):
+        """
+        Import teachers' data.
+
+        Returns:
+        Bool -  True if import successfully, 
+                false otherwise
+        """
+        self.teachers_salary = []
+        if os.path.exists(self.teachers_path):
+            with open(self.teachers_path, "r", encoding="utf8") as rf:
+                lines = rf.readlines()
+            for line in lines:
+                username, email, password, role, recept_id, salary, staff_info = line.strip("\n").split(",")
+                teacher_obj = TeacherSalary(username, email, recept_id, salary, staff_info)
+                self.teachers_salary.append(teacher_obj)
+            return True
+        else:
+            print(f"Please check the subdirectory and file exists for {self.teachers_path}.")
+            return False
+            
+    def teacher_salary(self):
+        """
+        Showing staff Salary
+        """
+        for teacher in self.teachers_salary:
+            print(teacher.username, teacher.email,  teacher.recept_id, teacher.salary, teacher.staff_info)
